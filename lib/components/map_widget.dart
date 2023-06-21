39af4b2aad37de6bf08fbeabc9843f7f41b29578
siddharthkanna <thesiddharthk@gmail.com>
2023-06-21 09:58:54 +0530

    display route

--- lib/components/map_widget.dart
+++ lib/components/map_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 final mapBoxAccessToken = dotenv.env['accessToken']!;
 final mapBoxStyleId = dotenv.env['styleId']!;
@@ -20,11 +22,12 @@ class MapWidget extends StatefulWidget {
   }) : super(key: key);
 
   @override
-   State<MapWidget> createState() => _MapWidgetState();
+  State<MapWidget> createState() => _MapWidgetState();
 }
 
 class _MapWidgetState extends State<MapWidget> with TickerProviderStateMixin {
   late MapController mapController;
+  List<LatLng> routeCoordinates = [];
 
   _MapWidgetState() : mapController = MapController();
 
@@ -35,11 +38,13 @@ class _MapWidgetState extends State<MapWidget> with TickerProviderStateMixin {
     // Check if pickup location has changed
     if (widget.pickupLocation != oldWidget.pickupLocation) {
       moveToPickupLocation();
+      getRouteCoordinates();
     }
 
     // Check if destination location has changed
     if (widget.destinationLocation != oldWidget.destinationLocation) {
       moveToDestinationLocation();
+      getRouteCoordinates();
     }
   }
 
@@ -57,6 +62,54 @@ class _MapWidgetState extends State<MapWidget> with TickerProviderStateMixin {
     }
   }
 
+  Future<void> getRouteCoordinates() async {
+    if (widget.pickupLocation != null && widget.destinationLocation != null) {
+      final response = await http.get(
+        Uri.parse(
+          "https://api.mapbox.com/directions/v5/mapbox/driving/${widget.pickupLocation!.longitude},${widget.pickupLocation!.latitude};${widget.destinationLocation!.longitude},${widget.destinationLocation!.latitude}?geometries=geojson&access_token=$mapBoxAccessToken",
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        final List<dynamic> coordinates =
+            data['routes'][0]['geometry']['coordinates'];
+        setState(() {
+          routeCoordinates =
+              coordinates.map((coord) => LatLng(coord[1], coord[0])).toList();
+          adjustMapZoom();
+        });
+      }
+    }
+  }
+
+  void adjustMapZoom() {
+    if (routeCoordinates.isNotEmpty) {
+      double minLat = double.infinity;
+      double maxLat = -double.infinity;
+      double minLng = double.infinity;
+      double maxLng = -double.infinity;
+      
+
+      for (final coord in routeCoordinates) {
+        if (coord.latitude < minLat) minLat = coord.latitude;
+        if (coord.latitude > maxLat) maxLat = coord.latitude;
+        if (coord.longitude < minLng) minLng = coord.longitude;
+        if (coord.longitude > maxLng) maxLng = coord.longitude;
+      }
+
+      final bounds = LatLngBounds(
+        LatLng(minLat, minLng),
+        LatLng(maxLat, maxLng),
+      );
+
+      mapController.fitBounds(
+        bounds,
+        options: const FitBoundsOptions(padding: EdgeInsets.all(110.0),),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return FlutterMap(
@@ -67,7 +120,8 @@ class _MapWidgetState extends State<MapWidget> with TickerProviderStateMixin {
         zoom: 13,
         center: widget.pickupLocation ?? myLocation,
         interactiveFlags: InteractiveFlag.pinchZoom |
-            InteractiveFlag.drag, // Disable rotation
+            InteractiveFlag.drag,
+             // Disable rotation
       ),
       children: [
         TileLayer(
@@ -77,6 +131,15 @@ class _MapWidgetState extends State<MapWidget> with TickerProviderStateMixin {
             'mapStyleId': mapBoxStyleId,
             'accessToken': mapBoxAccessToken,
           },
+        ),
+        PolylineLayer(
+          polylines: [
+            Polyline(
+              points: routeCoordinates,
+              strokeWidth: 4.0,
+              color: Colors.black54,
+            ),
+          ],
         ),
         MarkerLayer(
           markers: [
